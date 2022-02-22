68a3dea61b90e67a3ef423fbac5743b60e1dee02
winstonwang <wwang@lcs.on.ca>
2022-02-22 11:19:36 -0500

    Initial Commit

---
+++ DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Winston Wang on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
