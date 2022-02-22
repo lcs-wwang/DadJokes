a2142c887b7a58dbde0c475398013e7567eaebab
winstonwang <wwang@lcs.on.ca>
2022-02-22 12:10:51 -0500

    got the app talk to the end point and retrieve a new joke when app loads

--- DadJokes/ContentView.swift
+++ DadJokes/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     //MARK: Stored propertie
-    var currentJoke: DadJoke = DadJoke(id: "",
-                                       joke: "nigger",
+    @State var currentJoke: DadJoke = DadJoke(id: "",
+                                       joke: "black cock",
                                        status: 0)
     
     //MARK: Computed properties
@@ -51,6 +51,44 @@ struct ContentView: View {
             
             Spacer()
             
+            
+        }
+        // When the app opens, get a new joke from the web service
+        .task {
+            
+            // Assemble the URL that points to the endpoint
+            let url = URL(string: "https://icanhazdadjoke.com/")!
+            
+            // Define the type of data we want from the endpoint
+            // Configure the request to the web site
+            var request = URLRequest(url: url)
+            // Ask for JSON data
+            request.setValue("application/json",
+                             forHTTPHeaderField: "Accept")
+            
+            // Start a session to interact (talk with) the endpoint
+            let urlSession = URLSession.shared
+            
+            // Try to fetch a new joke
+            // It might not work, so we use a do-catch block
+            do {
+                
+                // Get the raw data from the endpoint
+                let (data, _) = try await urlSession.data(for: request)
+                
+                // Attempt to decode the raw data into a Swift structure
+                // Takes what is in "data" and tries to put it into "currentJoke"
+                //                                 DATA TYPE TO DECODE TO
+                //                                         |
+                //                                         V
+                currentJoke = try JSONDecoder().decode(DadJoke.self, from: data)
+                
+            } catch {
+                print("Could not retrieve or decode the JSON from endpoint.")
+                // Print the contents of the "error" constant that the do-catch block
+                // populates
+                print(error)
+            }
             
         }
         .navigationTitle("icanhazdadjoke?")
