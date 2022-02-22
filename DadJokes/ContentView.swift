f7a7d44872931e3e14bc7fa32e60f0c644f4b45f
winstonwang <wwang@lcs.on.ca>
2022-02-22 11:41:15 -0500

    laid out a static interface for the app

--- DadJokes/ContentView.swift
+++ DadJokes/ContentView.swift
@@ -2,20 +2,60 @@
 //  ContentView.swift
 //  DadJokes
 //
-//  Created by Winston Wang on 2022-02-22.
+//  Created by Russell Gordon on 2022-02-21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Text("How do you organize a space party? You planet.")
+                .multilineTextAlignment(.leading)
+                .padding(30)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.primary, lineWidth: 4)
+                )
+                .padding(10)
+            Image(systemName: "heart.circle")
+                .resizable()
+                .frame(width: 40, height: 40)
+            
+            Button(action:{
+                    print("dfgehr")
+            }, label: {
+                Text("Another One!!!")
+            })
+                .buttonStyle(.bordered)
+            
+            
+            HStack{
+                Text("Favourites")
+                Spacer()
+                    .padding()
+            }
+            List {
+                Text("Which side of the chicken has more feathers? The outside.")
+                Text("Why did the Clydesdale give the pony a glass of water? Because he was a little horse!")
+                Text("The great thing about stationery shops is they're always in the same place...")
+            }
+            
+            Spacer()
+            
+            
+        }
+        .navigationTitle("icanhazdadjoke?")
+        .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
